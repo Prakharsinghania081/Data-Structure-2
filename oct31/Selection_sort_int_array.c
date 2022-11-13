e59ae06c9708f9bd0cae40bb1a71f5b50b74e459
Prakharsinghania081 <103634268+Prakharsinghania081@users.noreply.github.com>
2022-11-14 02:48:01 +0530

    Update Selection_sort_int_array.c

--- oct31/Selection_sort_int_array.c
+++ oct31/Selection_sort_int_array.c
@@ -45,4 +45,9 @@ return 0;
  /*Sample output: 
  Input the total number of elements in array:5
 Enter the elements of array: 132 24 12 1 134
-The sorted array is:  1 12 24 132 134 */
+The sorted array is:  1 12 24 132 134 
+
+Here, we make the assumption that the first element is the minimum. 
+Then, we determine if there is an element lower than that in the remaining elements of the array.
+If so, we switch the assumed minimum and the actual minimum; otherwise, we move on to the following element and search for the subsequent lowest element. 
+The entire list becomes sorted in this manner. It also has time complexity of O(n) and space complexity of O(1).*/
