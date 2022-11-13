f3b1e97995dfa36b88da1be7c9c6a93bf10ce421
Prakharsinghania081 <103634268+Prakharsinghania081@users.noreply.github.com>
2022-11-14 01:13:06 +0530

    Add files via upload

---
+++ oct31/Selection_sort_int_array.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+int array[5];
+int position, temp;
+
+printf("Input 5 integers \n");
+for (int i = 0; i < 5; i++)
+scanf("%d", &array[i]);
+printf("Unsorted: ");
+for (int i = 0; i < 5; i++){
+printf("%d ", array[i]);}
+printf("\n");
+
+for (int k = 0; k < 4; k++) // finding minimum element 4 times
+{
+position = k;
+
+for (int j = k + 1; j < 5; j++)
+{
+if (array[position] > array[j])
+position = j;
+}
+if (position != k) //Swap
+{
+temp = array[k];
+array[k] = array[position];
+array[position] = temp;
+}
+printf("Pass %d: ",k+1);
+for (int i = 0; i < 5; i++){
+printf("%d ", array[i]);
+}
+printf("\n");
+}
+
+printf("Sorted list in ascending order:\n");
+
+for (int i = 0; i < 5; i++)
+printf("%d ", array[i]);
+
+return 0;
+}
