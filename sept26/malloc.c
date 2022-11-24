9fdb024d651462ca87f6bc53c25249f408acd09c
Prakharsinghania081 <103634268+Prakharsinghania081@users.noreply.github.com>
2022-11-24 19:29:31 +0530

    Update malloc.c

--- sept26/malloc.c
+++ sept26/malloc.c
@@ -1,1 +1,11 @@
+/* Following are the functions and their notation we will be using:
 
+malloc()	: malloc (number *sizeof(int));
+calloc()	: calloc (number, sizeof(int));
+realloc()	: realloc (pointer_name, number * sizeof(int));
+free()	:  free (pointer_name);
+
+
+8/
+
+//1. Malloc() : 
