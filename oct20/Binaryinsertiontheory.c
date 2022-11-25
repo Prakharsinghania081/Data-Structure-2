8db5506e36801542ff613132b9b95a8332ce0a50
Prakharsinghania081 <103634268+Prakharsinghania081@users.noreply.github.com>
2022-11-26 02:36:41 +0530

    Create Binaryinsertiontheory.c

---
+++ oct20/Binaryinsertiontheory.c
@@ -0,0 +1,25 @@
+ /*
+ 
+ 
+ 
+insertion theory:
+
+case 1: add in the leaf. 
+u go on search. say u want to insert 37, 
+37>19, right node
+37>23 right node 
+37> 24, right node 
+
+make a right node and insert the element 
+
+
+
+        19
+     /.    \ 
+   5         23 
+  /. \      / \ 
+ 1.    11   21 24.
+                \
+                 37 
+                 
+     o(loh(n)) complexity 
