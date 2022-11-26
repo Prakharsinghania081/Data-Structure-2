62a7fcc8b2ebefec57fe23297a5ed50eb1e73ada
Prakharsinghania081 <103634268+Prakharsinghania081@users.noreply.github.com>
2022-11-26 14:54:15 +0530

    Create mallocdynamic.c

---
+++ sept26/mallocdynamic.c
@@ -0,0 +1,14 @@
+/* There are two cases for dynamic memory allocation. 
+
+
+CASE 1: 
+Number of elements in array < indices/memory allocated 
+In this case, we are wasting the memory and we woll free the space not needed by elements. 
+
+CASE 2: 
+number of elements > memory allocation. 
+Memory can take 9 indices, but we have 11 elements in aray to be entered. So now we need to increase the length. 
+
+
+coding for case 1: 
+
