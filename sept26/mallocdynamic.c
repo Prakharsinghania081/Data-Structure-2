1ffd6aec521a13e16d396568c6121d9d209da4d7
Prakharsinghania081 <103634268+Prakharsinghania081@users.noreply.github.com>
2022-11-26 16:11:02 +0530

    Update mallocdynamic.c

--- sept26/mallocdynamic.c
+++ sept26/mallocdynamic.c
@@ -29,7 +29,70 @@ realloc:
 ptr = realloc(ptr, newSize);
 with the new size, new block of that many bytes is created.
 
+*/ 
 
+//playing around dynamic array for memory allocation when the number of elements in array is more than the memory allocated. 
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+    int* pointer; ///the pointer 
+ 
+    // 
+    int n;
+    printf("Input size of array:");
+    scanf("%d",&n);
+ 
+    // allocate mnemory using calloc. 
+    pointer = calloc (n, sizeof(int));
+    int i; 
+ //
+    if (pointer == NULL) 
+       {
+        printf("No memory allocated. It returns null pointer\n");
+    }
+    
+    else {
+
+        // if pointer has some memory this means memory has been allocated 
+        printf("Memory successfully allocated\n");
+        //now we add elements in the array. 
+        for(int i=0;i<n;i++)
+            pointer[i]=rand()%100;
+ 
+        // print the randomly allocated elements of the array 
+        printf("The array with random elements are: ");
+        for (i = 0; i < n; ++i) {
+            printf("%d, ", pointer[i]);
+        }
+ 
+        // get new array size 
+        int n;
+        printf("\n\Input new size of the array:");
+        scanf("%d" , &n); 
+        
+        // use realloc to allocate new memory 
+        pointer = realloc(pointer, n * sizeof(int));
+ 
+        printf("Memory successfully re-allocated\n");
+ 
+        // Get the new elements of the array
+         for(int i=0;i<n;i++)
+            pointer[i]=rand()%50;
+ 
+        // print new elements 
+        printf("The elements of the array are: ");
+        for (i = 0; i < n; ++i) {
+            printf("%d, ", pointer[i]);
+        }
+ 
+        free(pointer); //free the space. 
+    }
+ 
+    return 0;
+}
 
 
 
